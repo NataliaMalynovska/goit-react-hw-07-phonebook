eca7739bc4c473f2a1c3efda0f478af5e76ea8d5
NataliaMalynovska <natalimalinovska57@gmail.com>
2022-10-09 21:11:32 +0300

    add memo

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilter = state => state.filter.value;
 
-export const selecttVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilter(state);
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-};
+export const selecttVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
